3ea107280d00b9ab35342697f20a8f2b5c88e862
summer-543 <12.1piece.24@gmail.com>
2026-05-11 17:42:40 +0900

    一時ファイル削除

--- docs/sf_objects_collection/fix.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-const filePath = path.join(__dirname, 'sf_objects.html');
-let content = fs.readFileSync(filePath, 'utf8');
-
-content = content.replace(/\\`/g, '`').replace(/\\\$/g, '$');
-
-fs.writeFileSync(filePath, content, 'utf8');
